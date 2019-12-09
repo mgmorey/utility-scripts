ba54ed0f53f00e6b19a1e0618c4517f5f86c4c98
Michael G. Morey <mgmorey@gmail.com>
2019-12-09 12:00:44 -0800

    Add new script files restapi-functions.sh and restapi-parameters.sh.

---
+++ restapi-functions.sh
@@ -0,0 +1,488 @@
+# -*- Mode: Shell-script -*-
+
+# restapi-functions.sh: Python RESTful API functions
+# Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+APPLE_URL=http://www.apple.com/DTDs/PropertyList-1.0.dtd
+
+WAIT_DEFAULT=5
+WAIT_RESTART=10
+WAIT_SIGNAL=10
+
+abort_insufficient_permissions() {
+    cat <<-EOF >&2
+	$0: Write access required to update file or directory: $1.
+	$0: Insufficient access to complete the requested operation.
+	$0: Please try the operation again as a privileged user.
+	EOF
+    exit 1
+}
+
+check_permissions() (
+    for node; do
+	check_permissions_single "$node"
+    done
+)
+
+check_permissions_single() {
+    if [ -w "$1" ]; then
+	return 0
+    elif [ -e "$1" ]; then
+	abort_insufficient_permissions "$1"
+    else
+	check_permissions_single "$(dirname "$1")"
+    fi
+}
+
+control_app() {
+    assert [ $# -eq 1 ]
+    assert [ -n "$1" ]
+
+    case "${kernel_name=$(uname -s)}" in
+	(Darwin)
+	    control_app_via_launchd $1
+	    ;;
+	(*)
+	    control_app_via_systemd $1
+	    ;;
+    esac
+}
+
+control_app_via_launchctl() (
+    assert [ $# -eq 3 ]
+    assert [ -n "$1" ]
+    assert [ -n "$2" ]
+    assert [ -n "$3" ]
+
+    case $1 in
+	(load)
+	    $2 $3
+
+	    if [ $dryrun = true ]; then
+		check_permissions $3
+	    else
+		launchctl load $3
+	    fi
+	    ;;
+	(unload)
+	    if [ $dryrun = true ]; then
+		check_permissions $3
+	    elif [ -e $3 ]; then
+		launchctl unload $3
+	    fi
+
+	    $2 $3
+	    ;;
+    esac
+)
+
+control_app_via_launchd() {
+    assert [ $# -eq 1 ]
+    assert [ -n "$1" ]
+    target=$(get_launch_agent_target)
+
+    case $1 in
+	(disable)
+	    if [ "$(is_uwsgi_service)" = true -a -e $target ]; then
+		control_app_via_launchctl unload remove_files $target
+	    fi
+	    ;;
+	(enable)
+	    if [ "$(is_uwsgi_service)" = true -a ! -e $target ]; then
+		control_app_via_launchctl load generate_launch_agent $target
+	    fi
+	    ;;
+	(stop)
+	    if [ $dryrun = false ]; then
+		signal_app $WAIT_SIGNAL INT TERM KILL || true
+	    fi
+	    ;;
+    esac
+}
+
+control_app_via_systemd() {
+    assert [ $# -eq 1 ]
+    assert [ -n "$1" ]
+
+    case $1 in
+	(disable)
+	    remove_files $(get_symlinks)
+	    ;;
+	(enable)
+	    create_symlinks $APP_CONFIG ${UWSGI_APPDIRS-}
+	    ;;
+	(stop)
+	    if [ $dryrun = false ]; then
+		signal_app $WAIT_SIGNAL INT TERM KILL || true
+	    fi
+	    ;;
+    esac
+}
+
+control_service() {
+    assert [ $# -eq 2 ]
+    assert [ -n "$1" ]
+    assert [ -n "$2" ]
+
+    case "${kernel_name=$(uname -s)}" in
+	(Darwin)
+	    :
+	    ;;
+	(*)
+	    control_service_via_systemd $1 $2
+	    ;;
+    esac
+}
+
+control_service_via_systemd() {
+    assert [ $# -eq 2 ]
+    assert [ -n "$1" ]
+    assert [ -n "$2" ]
+
+    if [ $dryrun = true ]; then
+	return 0
+    fi
+
+    case $1 in
+	(enable|disable|restart)
+	    systemctl $1 $2
+	    ;;
+    esac
+}
+
+generate_launch_agent() (
+    assert [ $# -eq 1 ]
+    assert [ -n "$1" ]
+
+    if [ $dryrun = false ]; then
+	create_tmpfile
+	xmlfile=$tmpfile
+	cat <<-EOF >$xmlfile
+	<?xml version="1.0" encoding="UTF-8"?>
+	<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "$APPLE_URL">
+	<plist version="1.0">
+	  <dict>
+	    <key>Label</key>
+	    <string>local.$APP_NAME</string>
+	    <key>RunAtLoad</key>
+	    <true/>
+	    <key>KeepAlive</key>
+	    <true/>
+	    <key>ProgramArguments</key>
+	    <array>
+	        <string>$(get_uwsgi_binary_path)</string>
+	        <string>--ini</string>
+	        <string>$APP_CONFIG</string>
+	    </array>
+	    <key>WorkingDirectory</key>
+	    <string>$APP_VARDIR</string>
+	  </dict>
+	</plist>
+	EOF
+    else
+	xmlfile=
+    fi
+
+    install_file 644 "$xmlfile" $1
+)
+
+get_app_status() {
+    if is_app_installed; then
+	if is_app_running; then
+	    printf "%s\n" running
+	else
+	    printf "%s\n" stopped
+	fi
+    else
+	printf "%s\n" uninstalled
+    fi
+}
+
+get_awk_command() (
+    for awk in /usr/gnu/bin/awk /usr/bin/gawk /usr/bin/awk; do
+	if [ -x $awk ]; then
+	    printf "%s\n" "$awk"
+	    return 0
+	fi
+    done
+
+    return 1
+)
+
+get_launch_agent_label() {
+    printf "%s\n" "local.$APP_NAME"
+}
+
+get_launch_agent_target() {
+    printf "%s\n" "$HOME/Library/LaunchAgents/$(get_launch_agent_label).plist"
+}
+
+get_service_status() {
+    assert [ $# -eq 1 ]
+    assert [ -n "$1" ]
+
+    if [ "$(is_uwsgi_service)" = false ]; then
+	return 0
+    elif ! is_system_running; then
+	return 0
+    elif is_service_loaded $1; then
+	if is_service_running $1; then
+	    printf "%s\n" running
+	else
+	    printf "%s\n" stopped
+	fi
+    else
+	printf "%s\n" uninstalled
+    fi
+}
+
+get_symlinks() (
+    if [ -z "${UWSGI_APPDIRS-}" ]; then
+	return 0
+    elif [ -z "${UWSGI_ETCDIR-}" ]; then
+	return 0
+    elif [ ! -d $UWSGI_ETCDIR ]; then
+	return 0
+    else
+	for dir in $UWSGI_APPDIRS; do
+	    printf "%s\n" $UWSGI_ETCDIR/$dir/$APP_NAME.ini
+	done
+    fi
+)
+
+install_file() {
+    assert [ $# -eq 3 ]
+    assert [ -n "$3" ]
+
+    if [ $dryrun = true ]; then
+	check_permissions_single $3
+    else
+	assert [ -n "$1" ]
+	assert [ -n "$2" ]
+	assert [ -r $2 ]
+
+	if is_tmpfile $2; then
+	    printf "Generating file %s\n" "$3"
+	else
+	    printf "Installing file %s as %s\n" "$2" "$3"
+	fi
+
+	install -d -m 755 "$(dirname "$3")"
+	install -m $1 $2 $3
+    fi
+}
+
+is_app_installed() {
+    test -e $APP_CONFIG
+}
+
+is_app_running() {
+    if [ -r $APP_PIDFILE ]; then
+	pid=$(cat $APP_PIDFILE)
+
+	if [ -n "$pid" ]; then
+	    if ps -p $pid >/dev/null; then
+		return 0
+	    else
+		pid=
+	    fi
+	fi
+    else
+	pid=
+    fi
+
+    return 1
+}
+
+is_service_active() {
+    assert [ $# -eq 1 ]
+    assert [ -n "$1" ]
+
+    if ! is_system_running || ! is_service_loaded uwsgi; then
+	return 1
+    fi
+
+    active=$(systemctl show --property=ActiveState $1)
+
+    case "${active#ActiveState=}" in
+	(active)
+	    return 0
+	    ;;
+	(*)
+	    return 1
+	    ;;
+    esac
+}
+
+is_service_loaded() {
+    assert [ $# -eq 1 ]
+    assert [ -n "$1" ]
+
+    if ! is_system_running; then
+	return 1
+    fi
+
+    loaded=$(systemctl show --property=LoadState $1)
+
+    case "${loaded#LoadState=}" in
+	(loaded)
+	    return 0
+	    ;;
+	(*)
+	    return 1
+	    ;;
+    esac
+}
+
+is_service_running() {
+    assert [ $# -eq 1 ]
+    assert [ -n "$1" ]
+
+    if ! is_system_running || ! is_service_loaded $1; then
+	return 1
+    fi
+
+    if ! is_service_active $1; then
+	return 1
+    fi
+
+    state=$(systemctl show --property=SubState $1)
+
+    case "${state#SubState=}" in
+	(exited|running)
+	    return 0
+	    ;;
+	(*)
+	    return 1
+	    ;;
+    esac
+}
+
+is_system_running() {
+    running=$(systemctl is-system-running)
+
+    case "$running" in
+	(degraded|running|starting)
+	    return 0
+	    ;;
+	(*)
+	    return 1
+	    ;;
+    esac
+}
+
+is_tmpfile() {
+    printf "%s\n" ${tmpfiles-} | grep -q $1
+}
+
+print_elapsed_time() {
+    if [ $elapsed -eq 0 ]; then
+	return 0
+    fi
+
+    printf "Service %s %s in %d seconds\n" "$APP_NAME" "$1" "$elapsed"
+}
+
+print_table() {
+    awk=$(get_awk_command)
+
+    if [ -z "$awk" ]; then
+	abort "No suitable awk command found\n"
+    fi
+
+    $awk -f "$script_dir/print-table.awk" \
+	 -v border="${1-1}" \
+	 -v header="${2-}" \
+	 -v width="${COLUMNS-80}"
+}
+
+remove_files() {
+    if [ $# -eq 0 ]; then
+	return 0
+    fi
+
+    if [ $dryrun = true ]; then
+	check_permissions "$@"
+    else
+	printf "Removing %s\n" $(printf "%s\n" "$@" | sort -u)
+	/bin/rm -rf "$@"
+    fi
+}
+
+signal_process() {
+    assert [ $# -ge 3 ]
+    assert [ -n "$1" ]
+    assert [ -n "$2" ]
+    assert [ -n "$3" ]
+    assert [ $3 -ge 0 -a $3 -le 60 ]
+    printf "Sending SIG%s to process (PID: %s)\n" $1 $2
+
+    case $1 in
+	(HUP)
+	    signal_process_and_wait "$@"
+	    ;;
+	(*)
+	    signal_process_and_poll "$@"
+	    ;;
+    esac
+}
+
+signal_process_and_poll() {
+    assert [ $# -eq 3 ]
+    assert [ -n "$1" ]
+    assert [ -n "$2" ]
+    assert [ -n "$3" ]
+    assert [ $3 -ge 0 -a $3 -le 60 ]
+    i=0
+
+    while kill -s $1 $2 && [ $i -lt $3 ]; do
+	if [ $i -eq 0 ]; then
+	    printf "%s\n" "Waiting for process to exit"
+	fi
+
+	sleep 1
+	i=$((i + 1))
+    done
+
+    elapsed=$((elapsed + i))
+    test $i -lt $3
+}
+
+signal_process_and_wait() {
+    assert [ $# -eq 3 ]
+    assert [ -n "$1" ]
+    assert [ -n "$2" ]
+    assert [ -n "$3" ]
+    assert [ $3 -ge 0 -a $3 -le 60 ]
+
+    if kill -s $1 $2; then
+	printf "Waiting for process to handle SIG%s\n" "$1"
+	sleep $3
+	elapsed=$((elapsed + $3))
+	return 0
+    fi
+
+    return 1
+}
+
+wait_for_timeout() {
+    assert [ $# -eq 1 ]
+    assert [ -n "$1" ]
+
+    if [ $1 -gt 0 ]; then
+	sleep $1
+	printf "%s\n" "$1"
+    else
+	printf "%s\n" 0
+    fi
+}
---
+++ restapi-parameters.sh
@@ -0,0 +1,870 @@
+# -*- Mode: Shell-script -*-
+
+# restapi-parameters.sh: Python RESTful API parameters
+# Copyright (C) 2018  "Michael G. Morey" <mgmorey@gmail.com>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+APP_VARS="APP_DIR APP_GID APP_LOGFILE APP_PIDFILE APP_PLUGIN APP_PORT APP_UID \
+APP_VARDIR"
+
+AWK_FORMAT='NR == 1 || $%d == binary {print $0}\n'
+PLUGIN_FORMAT="python%s_plugin.so\n"
+
+UWSGI_BRANCH=uwsgi-2.0
+UWSGI_URL=https://github.com/unbit/uwsgi.git
+
+abort_not_supported() {
+    abort "%s: %s: %s not supported\n" "$0" "$PRETTY_NAME" "$*"
+}
+
+awk_uwsgi() {
+    assert [ $# -eq 1 ]
+    awk "$(printf "$AWK_FORMAT" $PS_COLUMN)" binary="$1"
+}
+
+configure_all() {
+    configure_baseline
+    configure_defaults
+}
+
+configure_baseline() {
+    eval $(get-os-release -x)
+    eval $(get-app-configuration --input app.ini)
+
+    case "$kernel_name" in
+	(CYGWIN_NT-*)
+	    case "$VERSION_ID" in
+		(10.0)
+		    configure_windows
+		    ;;
+		(*)
+		    abort_not_supported Release
+		    ;;
+	    esac
+	    ;;
+	(Linux|GNU)
+	    configure_gnu
+
+	    case "$ID" in
+		(debian|raspbian)
+		    case "$VERSION_ID" in
+			(10)
+			    configure_linux_debian_10
+			    ;;
+			(*)
+			    abort_not_supported Release
+			    ;;
+		    esac
+		    ;;
+		(ubuntu|neon)
+		    case "$VERSION_ID" in
+			(18.04)
+			    configure_linux_ubuntu_18_04
+			    ;;
+			(19.04)
+			    configure_linux_ubuntu_19_04
+			    ;;
+			(19.10)
+			    configure_linux_ubuntu_19_10
+			    ;;
+			(*)
+			    abort_not_supported Release
+			    ;;
+		    esac
+		    ;;
+		(linuxmint)
+		    case "$VERSION_ID" in
+			(19.2)
+			    configure_linux_ubuntu_18_04
+			    ;;
+			(*)
+			    abort_not_supported Release
+			    ;;
+		    esac
+		    ;;
+		(kali)
+		    case "$VERSION_ID" in
+			(2019.4)
+			    configure_linux_kali_2019_4
+			    ;;
+			(*)
+			    abort_not_supported Release
+			    ;;
+		    esac
+		    ;;
+		(opensuse-leap)
+		    case "$VERSION_ID" in
+			(15.1)
+			    configure_linux_opensuse_lp_15
+			    ;;
+			(*)
+			    abort_not_supported Release
+			    ;;
+		    esac
+		    ;;
+		(opensuse-tumbleweed)
+		    case "$VERSION_ID" in
+			(2019*)
+			    configure_linux_opensuse_tw
+			    ;;
+			(*)
+			    abort_not_supported Release
+			    ;;
+		    esac
+		    ;;
+		(fedora)
+		    case "$VERSION_ID" in
+			(31)
+			    configure_linux_fedora_31
+			    ;;
+			(*)
+			    abort_not_supported Release
+			    ;;
+		    esac
+		    ;;
+		(rhel|ol)
+		    case "$VERSION_ID" in
+			(7.[78])
+			    configure_linux_redhat_7
+			    ;;
+			(8.[012])
+			    configure_linux_redhat_8
+			    ;;
+			(*)
+			    abort_not_supported Release
+			    ;;
+		    esac
+		    ;;
+		(centos)
+		    case "$VERSION_ID" in
+			(7)
+			    configure_linux_redhat_7
+			    ;;
+			(*)
+			    abort_not_supported Release
+			    ;;
+		    esac
+		    ;;
+		(*)
+		    abort_not_supported Distro
+		    ;;
+	    esac
+	    ;;
+	(Darwin)
+	    configure_unix_bsd
+
+	    case "$VERSION_ID" in
+		(10.14.*)
+		    configure_unix_macos
+		    ;;
+		(*)
+		    abort_not_supported Release
+		    ;;
+	    esac
+	    ;;
+	(FreeBSD)
+	    configure_unix_bsd
+
+	    case "$VERSION_ID" in
+		(11.*)
+		    configure_unix_freebsd_11
+		    ;;
+		(12.*)
+		    configure_unix_freebsd_12
+		    ;;
+		(*)
+		    abort_not_supported Release
+		    ;;
+	    esac
+	    ;;
+	(NetBSD)
+	    configure_unix_bsd
+
+	    case "$VERSION_ID" in
+		(8.1)
+		    configure_unix_netbsd
+		    ;;
+		(*)
+		    abort_not_supported Release
+		    ;;
+	    esac
+	    ;;
+	(SunOS)
+	    configure_unix
+
+	    case $ID in
+		(illumos)
+		    case "$VERSION_ID" in
+			(2019.10)
+			    configure_unix_illumos
+			    ;;
+			(*)
+			    abort_not_supported Release
+			    ;;
+		    esac
+		    ;;
+		(solaris)
+		    case "$VERSION_ID" in
+			(11.4)
+			    configure_unix_solaris
+			    ;;
+			(*)
+			    abort_not_supported Release
+			    ;;
+		    esac
+		    ;;
+		(*)
+		    abort_not_supported Distro
+		    ;;
+	    esac
+	    ;;
+	(*)
+	    abort_not_supported "Operating system"
+	    ;;
+    esac
+
+    # Set application group and user accounts
+
+    if [ -z "${APP_GID-}" ]; then
+	APP_GID=uwsgi
+    fi
+
+    if [ -z "${APP_UID-}" ]; then
+	APP_UID=uwsgi
+    fi
+
+    # Set application directories from APP_NAME and APP_PREFIX
+    APP_DIR=${APP_PREFIX-}/opt/$APP_NAME
+    APP_ETCDIR=${APP_PREFIX-}/etc/opt/$APP_NAME
+
+    if [ -z "${APP_VARDIR-}" ]; then
+	APP_VARDIR=${APP_PREFIX-}/var/opt/$APP_NAME
+    fi
+
+    # Set additional app file and directory parameters
+
+    APP_CONFIG=$APP_ETCDIR/app.ini
+
+    if [ -z "${APP_LOGDIR-}" ]; then
+	APP_LOGDIR=$APP_VARDIR
+    fi
+
+    if [ -z "${APP_RUNDIR-}" ]; then
+	APP_RUNDIR=$APP_VARDIR
+    fi
+
+    # Set additional file parameters from app directories
+
+    if [ -z "${APP_LOGFILE-}" ]; then
+	APP_LOGFILE=$APP_LOGDIR/$APP_NAME.log
+    fi
+
+    if [ -z "${APP_PIDFILE-}" ]; then
+	APP_PIDFILE=$APP_RUNDIR/$APP_NAME.pid
+    fi
+
+    # Set uWSGI-related parameters
+
+    if [ -z "${UWSGI_BUILDCONF-}" ]; then
+	UWSGI_BUILDCONF=core
+    fi
+
+    if [ -z "${UWSGI_ORIGIN-}" ]; then
+	UWSGI_ORIGIN=distro
+    fi
+
+    # Set uWSGI directory prefix
+    if [ -z "${UWSGI_PREFIX-}" ]; then
+	UWSGI_PREFIX=
+    fi
+
+    if [ -z "${UWSGI_ETCDIR-}" ]; then
+	if [ "$(is_uwsgi_service)" = true ]; then
+	    UWSGI_ETCDIR=${UWSGI_PREFIX:-}/etc/uwsgi
+	fi
+    fi
+
+    if [ -z "${UWSGI_BINARY_DIR-}" ]; then
+	UWSGI_BINARY_DIR=${UWSGI_PREFIX:-/usr}/bin
+    fi
+
+    if [ -z "${UWSGI_BINARY_NAME-}" ]; then
+	UWSGI_BINARY_NAME=uwsgi
+    fi
+}
+
+configure_defaults() {
+    # Set Python-related parameters
+
+    if [ -z "${SYSTEM_PYTHON-}" -o -z "${SYSTEM_PYTHON_VERSION-}" ]; then
+	if [ -n "${SYSTEM_PYTHON-}" ]; then
+	    SYSTEM_PYTHON_VERSION="$(get_python_version $SYSTEM_PYTHON)"
+	else
+	    python_triple=$(find_system_python)
+	    version_pair="${python_triple#* }"
+	    SYSTEM_PYTHON="${python_triple%% *}"
+	    SYSTEM_PYTHON_VERSION="${version_pair#* }"
+	fi
+
+	if ! check_python $SYSTEM_PYTHON $SYSTEM_PYTHON_VERSION; then
+	    abort "%s\n" "No suitable Python interpreter found"
+	fi
+    fi
+
+    # Set uWSGI-related parameters
+
+    if [ -z "${UWSGI_HAS_PLUGIN-}" ]; then
+	if [ "$UWSGI_BUILDCONF" = pyonly ]; then
+	    UWSGI_HAS_PLUGIN=false
+	elif [ "$UWSGI_ORIGIN" = pkgsrc ]; then
+	    UWSGI_HAS_PLUGIN=false
+	else
+	    UWSGI_HAS_PLUGIN=true
+	fi
+    fi
+
+    if [ -z "${UWSGI_BINARY_DIR-}" ]; then
+	UWSGI_BINARY_DIR=${UWSGI_PREFIX:-/usr}/bin
+    fi
+
+    if [ -z "${UWSGI_BINARY_NAME-}" ]; then
+	UWSGI_BINARY_NAME=uwsgi
+    fi
+
+    if [ -z "${UWSGI_PLUGIN_DIR-}" ]; then
+	UWSGI_PLUGIN_DIR=${UWSGI_PREFIX:-/usr}/lib/uwsgi/plugins
+    fi
+
+    if [ -z "${UWSGI_PLUGIN_NAME-}" ]; then
+	UWSGI_PLUGIN_NAME=$(find_uwsgi_plugin)
+    fi
+
+    # Set app plugin from uWSGI plugin filename
+    if [ -z "${APP_PLUGIN-}" -a -n "${UWSGI_PLUGIN_NAME-}" ]; then
+	APP_PLUGIN=${UWSGI_PLUGIN_NAME%_plugin.so}
+    fi
+
+    # Set additional file parameters from app directories
+
+    if [ -z "${APP_PIDFILE-}" ]; then
+	APP_PIDFILE=$APP_RUNDIR/$APP_NAME.pid
+    fi
+
+    if [ -z "${APP_SOCKET-}" ]; then
+	APP_SOCKET=
+    fi
+}
+
+configure_from_source() {
+    # Set uWSGI prefix directory
+    UWSGI_PREFIX=/usr/local
+
+    # Set other uWSGI parameters
+
+    if [ -z "${UWSGI_BUILDCONF-}" ]; then
+	UWSGI_BUILDCONF=pyonly
+    fi
+
+    UWSGI_ORIGIN=source
+}
+
+configure_gnu() {
+    # Set ps command format and command column
+    PS_COLUMN=10
+    PS_FORMAT=pid,ppid,user,tt,lstart,command
+}
+
+configure_linux_debian() {
+    # Set application group and user accounts
+    APP_GID=www-data
+    APP_UID=www-data
+
+    # Set additional file/directory parameters
+    APP_LOGDIR=/var/log/uwsgi/app
+    APP_RUNDIR=/var/run/uwsgi/app/$APP_NAME
+
+    # Set additional parameters from app directories
+    APP_PIDFILE=$APP_RUNDIR/pid
+    APP_SOCKET=$APP_RUNDIR/socket
+
+    # Set uWSGI configuration directories
+    UWSGI_APPDIRS="apps-available apps-enabled"
+}
+
+configure_linux_debian_10() {
+    configure_linux_debian
+
+    # Set system Python interpreter
+    SYSTEM_PYTHON=/usr/bin/python3.7
+    SYSTEM_PYTHON_VERSION=3.7.3
+}
+
+configure_linux_fedora() {
+    # Set uWSGI configuration directory
+    UWSGI_ETCDIR=/etc
+
+    # Set uWSGI app configuration directory
+    UWSGI_APPDIRS=uwsgi.d
+
+    # Set uWSGI binary/plugin directories
+    UWSGI_BINARY_DIR=/usr/sbin
+    UWSGI_PLUGIN_DIR=/usr/lib64/uwsgi
+}
+
+configure_linux_fedora_31() {
+    configure_linux_fedora
+}
+
+configure_linux_kali_2019_4() {
+    configure_linux_debian
+
+    # Set system Python interpreter
+    SYSTEM_PYTHON=/usr/bin/python3.7
+    SYSTEM_PYTHON_VERSION=3.7.4
+}
+
+configure_linux_opensuse() {
+    # Set application group and user accounts
+    APP_GID=nogroup
+    APP_UID=nobody
+
+    # Set uWSGI configuration directories
+    UWSGI_APPDIRS=vassals
+
+    # Set uWSGI binary/plugin directories
+    UWSGI_BINARY_DIR=/usr/sbin
+    UWSGI_PLUGIN_DIR=/usr/lib64/uwsgi
+}
+
+configure_linux_opensuse_lp_15() {
+    configure_linux_opensuse
+
+    # Set system Python interpreter
+    SYSTEM_PYTHON=/usr/bin/python3.6
+    SYSTEM_PYTHON_VERSION=3.6.5
+}
+
+configure_linux_opensuse_tw() {
+    configure_linux_opensuse
+
+    # Set system Python interpreter
+    SYSTEM_PYTHON=/usr/bin/python3
+}
+
+configure_linux_redhat() {
+    # Set application group and user accounts
+    APP_GID=nobody
+    APP_UID=nobody
+}
+
+configure_linux_redhat_7() {
+    configure_linux_redhat
+
+    # Set system Python interpreter
+    SYSTEM_PYTHON=/usr/pkg/bin/python3.7
+
+    # Set uWSGI prefix directory
+    UWSGI_PREFIX=/usr/pkg
+
+    # Set uWSGI binary file
+    UWSGI_BINARY_NAME=uwsgi-3.7
+
+    # Set other uWSGI parameters
+    UWSGI_HAS_PLUGIN=false
+    UWSGI_ORIGIN=pkgsrc
+}
+
+configure_linux_redhat_8() {
+    configure_linux_redhat
+
+    # Set uWSGI binary/plugin directories
+    UWSGI_BINARY_DIR=/usr/local/bin
+
+    # Set uWSGI parameters
+    UWSGI_HAS_PLUGIN=false
+    UWSGI_ORIGIN=pypi
+}
+
+configure_linux_ubuntu_18_04() {
+    configure_linux_debian
+
+    # Set system Python interpreter
+    SYSTEM_PYTHON=/usr/bin/python3.6
+    SYSTEM_PYTHON_VERSION=3.6.8
+}
+
+configure_linux_ubuntu_19_04() {
+    configure_linux_debian
+
+    # Set system Python interpreter
+    SYSTEM_PYTHON=/usr/bin/python3.7
+}
+
+configure_linux_ubuntu_19_10() {
+    configure_linux_debian
+
+    # Set system Python interpreter
+    SYSTEM_PYTHON=/usr/bin/python3.7
+}
+
+configure_unix() {
+    # Set ps command format and column number
+    PS_COLUMN=6
+    PS_FORMAT=pid,ppid,user,tty,stime,args
+}
+
+configure_unix_bsd() {
+    # Set ps command format and column number
+    PS_COLUMN=10
+    PS_FORMAT=pid,ppid,user,tt,lstart,command
+}
+
+configure_unix_freebsd() {
+    # Set uWSGI prefix directory
+    UWSGI_PREFIX=/usr/local
+
+    # Set uWSGI binary file
+    UWSGI_BINARY_NAME=uwsgi-3.6
+
+    # Set other uWSGI parameters
+    UWSGI_HAS_PLUGIN=false
+}
+
+configure_unix_freebsd_11() {
+    configure_unix_freebsd
+}
+
+configure_unix_freebsd_12() {
+    configure_unix_freebsd
+}
+
+configure_unix_illumos() {
+    # Set application group and user accounts
+    APP_GID=webservd
+    APP_UID=webservd
+
+    # Set system Python interpreter
+    SYSTEM_PYTHON=/opt/local/bin/python3.7
+
+    # Set uWSGI prefix directory
+    UWSGI_PREFIX=/opt/local
+
+    # Set uWSGI binary file
+    UWSGI_BINARY_NAME=uwsgi-3.7
+
+    # Set other uWSGI parameters
+    UWSGI_CC=gcc
+    UWSGI_CFLAGS=-m64
+    UWSGI_ORIGIN=pkgsrc
+}
+
+configure_unix_macos() {
+    # Set application group and user accounts
+    APP_GID=_www
+    APP_UID=_www
+
+    # Set system Python interpreter
+    SYSTEM_PYTHON=/opt/pkg/bin/python3.7
+
+    # Set uWSGI prefix directory
+    UWSGI_PREFIX=/opt/pkg
+
+    # Set uWSGI binary file
+    UWSGI_BINARY_NAME=uwsgi-3.7
+
+    # Set other uWSGI parameters
+    UWSGI_ORIGIN=pkgsrc
+}
+
+configure_unix_netbsd() {
+    # Set application group and user accounts
+    APP_GID=www
+    APP_UID=www
+
+    # Set system Python interpreter
+    SYSTEM_PYTHON=/usr/pkg/bin/python3.7
+
+    # Set uWSGI prefix directory
+    UWSGI_PREFIX=/usr/pkg
+
+    # Set uWSGI binary file
+    UWSGI_BINARY_NAME=uwsgi-3.7
+
+    # Set other uWSGI parameters
+    UWSGI_ORIGIN=pkgsrc
+}
+
+configure_unix_solaris() {
+    # Set application group and user accounts
+    APP_GID=webservd
+    APP_UID=webservd
+
+    # Set uWSGI parameters
+    configure_from_source
+}
+
+configure_windows() {
+    # Set ps command format and command column
+    PS_COLUMN=6
+
+    # Set uWSGI binary/plugin directories
+    UWSGI_BINARY_DIR=/usr/bin
+
+    # Set uWSGI parameters
+    UWSGI_HAS_PLUGIN=false
+    UWSGI_ORIGIN=pypi
+}
+
+find_available_plugins() {
+    printf $PLUGIN_FORMAT $(find_system_pythons | awk '{print $2}' | tr -d .)
+}
+
+find_installed_plugins() {
+    cd $UWSGI_PLUGIN_DIR 2>/dev/null && ls "$@" 2>/dev/null || true
+}
+
+find_plugins() (
+    available_plugins="$(find_available_plugins)"
+    installed_plugins="$(find_installed_plugins $available_plugins)"
+
+    if [ -n "$installed_plugins" ]; then
+	printf "%s\n" $installed_plugins
+    else
+	printf "%s\n" $available_plugins
+    fi
+)
+
+find_uwsgi_plugin() {
+    if [ $UWSGI_HAS_PLUGIN = true ]; then
+	find_plugins | head -n 1
+    fi
+}
+
+get_service_processes() {
+    ps_uwsgi $(get_service_users) | awk_uwsgi $(get_uwsgi_binary_path)
+}
+
+get_service_users() {
+    case "$kernel_name" in
+	(Linux)
+	    case "$ID" in
+		(opensuse-*)
+		    printf "%s\n" $APP_UID,root
+		    ;;
+		(*)
+		    printf "%s\n" $APP_UID
+		    ;;
+	    esac
+	    ;;
+	(*)
+	    printf "%s\n" $APP_UID
+	    ;;
+    esac
+}
+
+get_uwsgi_binary_path() {
+    printf "%s/%s\n" "$UWSGI_BINARY_DIR" "$UWSGI_BINARY_NAME"
+}
+
+get_uwsgi_plugin_path() {
+    if [ -n "${UWSGI_PLUGIN_DIR-}" -a -n "${UWSGI_PLUGIN_NAME-}" ]; then
+	printf "%s/%s\n" "$UWSGI_PLUGIN_DIR" "$UWSGI_PLUGIN_NAME"
+    fi
+}
+
+get_uwsgi_version() {
+    uwsgi=$(get_uwsgi_binary_path)
+
+    if [ -n "$uwsgi" ] && [ -x $uwsgi ]; then
+	$uwsgi --version
+    else
+	printf "%s\n" "<none>"
+    fi
+}
+
+is_uwsgi_packaged() {
+    case "$UWSGI_ORIGIN" in
+	(distro)
+ 	    is_packaged=true
+	    ;;
+	(pkgsrc)
+	    is_packaged=true
+	    ;;
+	(pypi)
+	    is_packaged=true
+	    ;;
+	(source)
+	    is_packaged=false
+	    ;;
+	(*)
+	    is_packaged=false
+	    ;;
+    esac
+
+    printf "%s\n" "$is_packaged"
+}
+
+is_uwsgi_service() {
+    case "$kernel_name" in
+	(GNU|FreeBSD)
+	    is_service=false
+	    ;;
+	(*)
+	    case "$UWSGI_ORIGIN" in
+		(distro)
+		    is_service=true
+		    ;;
+		(pkgsrc)
+		    is_service=false
+		    ;;
+		(pypi)
+		    is_service=false
+		    ;;
+		(source)
+		    is_service=false
+		    ;;
+		(*)
+		    is_service=false
+		    ;;
+	    esac
+	    ;;
+    esac
+
+    printf "%s\n" "$is_service"
+}
+
+print_app_log_file() {
+    assert [ $# -le 1 ]
+
+    if [ -r $APP_LOGFILE ]; then
+	rows="${ROWS-10}"
+	header="SERVICE LOG $APP_LOGFILE (last $rows lines)"
+	tail -n "$rows" $APP_LOGFILE | print_table "${1-1}" "$header"
+    elif [ -e $APP_LOGFILE ]; then
+	printf "%s: No read permission\n" "$APP_LOGFILE" >&2
+    fi
+}
+
+print_app_processes() {
+    get_service_processes | print_table ${1-} ""
+}
+
+ps_uwsgi() {
+    case "$kernel_name" in
+	(CYGWIN_NT-*)
+	    ps -ef
+	    ;;
+	(*)
+	    ps -U "$1" -o $PS_FORMAT
+	    ;;
+    esac
+}
+
+signal_app() {
+    assert [ $# -ge 1 ]
+    assert [ -n "$1" ]
+    assert [ $1 -gt 0 ]
+    wait=$1
+    shift
+    elapsed=0
+
+    if [ -z "${pid-}" ]; then
+	if [ -r $APP_PIDFILE ]; then
+	    pid=$(cat $APP_PIDFILE 2>/dev/null)
+	else
+	    pid=
+	fi
+    fi
+
+    if [ -z "$pid" ]; then
+	return 1
+    fi
+
+    for signal in "$@"; do
+	if signal_process "$signal" "$pid" "$wait"; then
+	    return 0
+	fi
+    done
+
+    return 1
+}
+
+signal_app_restart() {
+    elapsed=0
+
+    if is_app_running && signal_app $WAIT_SIGNAL HUP; then
+	elapsed=$((elapsed + $(wait_for_timeout $((WAIT_RESTART - elapsed)))))
+	restart_requested=true
+    else
+	restart_requested=false
+    fi
+}
+
+validate_parameters_postinstallation() {
+    if [ ! -d $APP_ETCDIR ]; then
+	abort "%s: %s: No such configuration directory\n" "$0" "$APP_ETCDIR"
+    elif [ ! -e $APP_CONFIG ]; then
+	abort "%s: %s: No such configuration file\n" "$0" "$APP_CONFIG"
+    elif [ ! -r $APP_CONFIG ]; then
+	abort "%s: %s: No read permission\n" "$0" "$APP_CONFIG"
+    elif [ ! -d $APP_DIR ]; then
+	abort "%s: %s: No such program directory\n" "$0" "$APP_DIR"
+    elif [ ! -d $APP_VARDIR ]; then
+	abort "%s: %s: No such working directory\n" "$0" "$APP_VARDIR"
+    elif [ ! -d $APP_LOGDIR ]; then
+	abort "%s: %s: No such log directory\n" "$0" "$APP_LOGDIR"
+    elif [ ! -d $APP_RUNDIR ]; then
+	abort "%s: %s: No such run directory\n" "$0" "$APP_RUNDIR"
+    elif [ -e $APP_LOGFILE -a ! -w $APP_LOGFILE ]; then
+	abort_insufficient_permissions "$APP_LOGFILE"
+    elif [ -e $APP_PIDFILE -a ! -w $APP_PIDFILE ]; then
+	abort_insufficient_permissions "$APP_PIDFILE"
+    fi
+}
+
+validate_parameters_preinstallation() {
+    binary=$(get_uwsgi_binary_path)
+    plugin=$(get_uwsgi_plugin_path)
+
+    if [ ! -e $binary ]; then
+	abort "%s: %s: No such binary file\n" "$0" "$binary"
+    elif [ ! -x $binary ]; then
+	abort "%s: %s: No execute permission\n" "$0" "$binary"
+    elif ! $binary --version >/dev/null 2>&1; then
+	abort "%s: %s: Unable to query version\n" "$0" "$binary"
+    elif [ $UWSGI_HAS_PLUGIN = true ]; then
+	if [ ! -e $plugin ]; then
+	    abort "%s: %s: No such plugin file\n" "$0" "$plugin"
+	elif [ ! -r $plugin ]; then
+	    abort "%s: %s: No read permission\n" "$0" "$plugin"
+	fi
+    fi
+}
+
+wait_for_service() {
+    assert [ $# -eq 1 ]
+    assert [ -n "$1" ]
+    i=0
+
+    if [ $1 -gt 0 ]; then
+	while [ ! -e $APP_PIDFILE -a $i -lt $1 ]; do
+	    sleep 1
+	    i=$((i + 1))
+	done
+    fi
+
+    if [ $i -ge $1 ]; then
+	printf "Service failed to start within %s seconds\n" $1 >&2
+    fi
+
+    printf "%s\n" "$i"
+}
