f9410f8bb7195ada847a0c7977c5010b625641a5
Michael G. Morey <mgmorey@gmail.com>
2020-04-18 18:27:44 +0000

    Remove parameter DIRS and rename ODBC driver-related parameters.

--- oracle-config.sh
+++ oracle-config.sh
@@ -1,5 +1,7 @@
 # tableau-config.sh: define shell parameters for common values
+CLIENT_VERSION=19.6
 DATABASES=""
-DIRS="$ORACLE_BASE $ORACLE_HOME"
-DRIVER="Oracle 19 ODBC driver"
+DRIVER_DESC='Oracle ODBC driver for Oracle 19'
+DRIVER_NAME='Oracle 19 ODBC driver'
+DRIVER_OBJECT=/usr/lib/oracle/$CLIENT_VERSION/client64/lib/libsqora.so.19.1
 ENV_FILE=.env
