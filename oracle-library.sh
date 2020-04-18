96e147ab5493df7b7c9c91d102a710bd28c886f9
Michael G. Morey <mgmorey@gmail.com>
2020-04-17 20:07:45 -0700

    Add new shell library oracle-library.sh.

---
+++ oracle-library.sh
@@ -0,0 +1,76 @@
+# tableau-library.sh: define shell functions for common logic
+
+_get_date_iso_8601() {
+    local ts=$(date --iso-8601=seconds)
+    printf '%s\n' "${ts:0:22}${ts:23}"
+}
+
+_print() {
+    local level=$1; shift
+    local timestamp=$(_get_date_iso_8601)
+    printf '%s [%s] [%s] %s\n' "$timestamp" "$level" "$script_basename" "$*"
+}
+
+abort() {
+    print_error "$@"
+    exit 1
+}
+
+assert() {
+    "$@" || abort "Assertion failed: $*"
+}
+
+check_dirs() {
+    for dir in $DIRS; do
+	if [ ! -d $dir ]; then
+	    abort '%s: %s: No such directory\n' "$0" "$dir"
+	fi
+    done
+}
+
+create_tmpfile() {
+    tmpfile=$(mktemp)
+    assert [ -n "${tmpfile}" ]
+    tmpfiles="${tmpfiles+$tmpfiles }$tmpfile"
+    trap "/bin/rm -f $tmpfiles" EXIT INT QUIT TERM
+}
+
+filter_db_name() {
+    cut -c 1-8
+}
+
+filter_sid() {
+    tr -cd '[:alnum:]_' | uppercase | cut -c 1-12
+}
+
+get_db_name() {
+    assert [ $# -eq 1 ]
+    assert [ -n "$1" ]
+    printf '%s\n' "$1" | filter_db_name
+}
+
+get_system_id() {
+    assert [ $# -eq 1 ]
+    assert [ -n "$1" ]
+    printf '%s\n' "$1" | filter_sid
+}
+
+print_debug() {
+    _print DEBUG "$@"
+}
+
+print_error() {
+    _print ERROR "$@" >&2
+}
+
+print_info() {
+    _print INFO "$@"
+}
+
+print_warn() {
+    _print WARNING "$@" >&2
+}
+
+uppercase() {
+    tr '[:lower:]' '[:upper:]'
+}
