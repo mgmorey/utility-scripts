1fbc5718df8e60c955c195a00c86e25cac9a6244
Michael G. Morey <mgmorey@gmail.com>
2020-04-28 15:57:33 -0700

    Add new config file sap-hana-config.sh.

---
+++ sap-hana-config.sh
@@ -0,0 +1,7 @@
+# oracle-config.sh: define shell parameters for common values
+CLIENT_VERSION=19.6
+DATABASES=""
+DRIVER_DESC='Oracle ODBC driver for Oracle 19'
+DRIVER_NAME='Oracle 19 ODBC driver'
+DRIVER_OBJECT=/usr/lib/oracle/$CLIENT_VERSION/client64/lib/libsqora.so.19.1
+ENV_FILE=.env
