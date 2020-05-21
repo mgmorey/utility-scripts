67448bfab86600e23de0a89264745278e0b9d016
Michael G. Morey <mgmorey@gmail.com>
2020-05-21 09:21:57 -0700

    Add and rename parameters in configuration file sap-hana-config.sh.

--- sap-hana-config.sh
+++ sap-hana-config.sh
@@ -1,5 +1,7 @@
 # sap-hana-config.sh: define shell parameters for common values
 DATADIR=/data/sap-hana
-IMAGE=store/saplabs/hanaexpress
-JSONFILE=sap-hana.json
-TAG=2.00.045.00.20200121.1
+ENV_FILE=$HOME/.env
+IMAGE_NAME=store/saplabs/hanaexpress
+IMAGE_TAG=2.00.045.00.20200121.1
+JSON_FILE=sap-hana.json
+PORTS="1128-1129 39013 39041-39045 59013-59014"
