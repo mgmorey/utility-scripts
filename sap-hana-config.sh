ffbae3b1728cd3389deb2affee9981db4c468517
Michael G. Morey <mgmorey@gmail.com>
2020-05-28 08:47:02 -0700

    Remove parameters DATADIR and JSON_FILE from configuration file.

--- sap-hana-config.sh
+++ sap-hana-config.sh
@@ -1,8 +1,6 @@
 # sap-hana-config.sh: define shell parameters for common values
 DATABASES="TEST_SPECIAL TestV1"
-DATADIR=/data/sap-hana
 ENV_FILE=$HOME/.env
-IMAGE_NAME=store/saplabs/hanaexpress
-IMAGE_TAG=2.00.045.00.20200121.1
-JSON_FILE=sap-hana.json
+IMAGE_NAME=tableau-sap-hana-ee2
+IMAGE_TAG=latest
 PORTS="1128-1129 39013 39041-39045 59013-59014"
