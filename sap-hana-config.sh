88958b4827a91de9c1aed89a2e390fcb17d1fb99
Michael G. Morey <mgmorey@gmail.com>
2020-05-29 09:28:32 -0700

    Restore original SAP HANA image name and command line.

--- sap-hana-config.sh
+++ sap-hana-config.sh
@@ -1,6 +1,6 @@
 # sap-hana-config.sh: define shell parameters for common values
 DATABASES="TEST_SPECIAL TestV1"
 ENV_FILE=$HOME/.env
-IMAGE_NAME=tableau-sap-hana-ee2
-IMAGE_TAG=latest
+IMAGE_NAME=store/saplabs/hanaexpress
+IMAGE_TAG=2.00.045.00.20200121.1
 PORTS="1128-1129 39013 39017 39041-39045 59013-59014"
