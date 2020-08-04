376a40d6de2e104a95647d846a948b46578f6cb1
Michael G. Morey <mgmorey@gmail.com>
2020-08-04 11:29:40 -0700

    Refactor Docker container scripts to enable additional flexibility.

--- sap-hana-config.sh
+++ sap-hana-config.sh
@@ -1,6 +1,4 @@
 # sap-hana-config.sh: define shell parameters for common values
-DATABASES="TEST_SPECIAL TestV1"
-ENV_FILE=$HOME/.env
 IMAGE_NAME=store/saplabs/hanaexpress
 IMAGE_TAG=2.00.045.00.20200121.1
 PORTS="1128-1129 39013 39017 39041-39045 59013-59014"
