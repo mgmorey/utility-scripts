7352cc4ded9e10ac15c9a1f6c7ec25933b90372f
Michael G. Morey <mgmorey@gmail.com>
2020-08-06 06:57:44 -0700

    Require command line argument for image name in docker-run scripts.

--- ibm-db2-config.sh
+++ ibm-db2-config.sh
@@ -1,4 +1,2 @@
 # ibm-db2-config.sh: define shell parameters for common values
-IMAGE_NAME=ibmcom/db2
-IMAGE_TAG=11.5.4.0
 PORTS="50000"
--- sap-hana-config.sh
+++ sap-hana-config.sh
@@ -1,4 +1,2 @@
 # sap-hana-config.sh: define shell parameters for common values
-IMAGE_NAME=store/saplabs/hanaexpress
-IMAGE_TAG=2.00.045.00.20200121.1
 PORTS="1128-1129 39013 39017 39041-39045 59013-59014"
